7c6cc6578d688e4885dea7482146b6954a618ac2
aleajactaesgf <aleajactaesgf@gmail.com>
2020-10-08 15:55:15 +0200

    docs agregado

---
+++ docs/precache-manifest.fd557811507d47cb5fae80fbfc6b1c18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b77199bdbed097ba89f5d48a5c23294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd8133ff01a7a0f96e6d",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8133ff01a7a0f96e6d",
+    "url": "/static/js/main.2fc010b1.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
